661ffc36d76ca60264af10d531282c3d9f65460b
Jes <jascaevola@gmail.com>
2023-08-22 03:33:01 +0100

    Update Navbar.tsx
    github link

--- src/partials/Navbar.tsx
+++ src/partials/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => (
 
       <NavMenu>
         <NavMenuItem href="/posts/">Blogs</NavMenuItem>
-        <NavMenuItem href="/">GitHub</NavMenuItem>
+        <NavMenuItem href="https://github.com/JAS-315">GitHub</NavMenuItem>
         <NavMenuItem href="/">Twitter</NavMenuItem>
       </NavMenu>
     </NavbarTwoColumns>
